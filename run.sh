3213e7d5ab0e24d8d4a38d713e1cb45172dc24a1
Hari Narayani Vengadessaper <harinarayani.v@zucisystems.com>
2024-12-30 19:19:21 +0530

    Updated test results folder path in run.sh file

--- run.sh
+++ run.sh
@@ -21,6 +21,9 @@ ALLURE_REPORT="./output/allure-report"
 mkdir -p "$ALLURE_RESULTS"
 mkdir -p "$ALLURE_REPORT"
 
+mkdir -p /selenium-web-pom/test-results
+mkdir -p /selenium-api/test-results
+
 # Run web tests with Allure reporting
 echo "Running Web Tests..."
 pabot --processes $PROCESSES \
